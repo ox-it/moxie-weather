981c3aeae4b2b4f8f2107a7c51ab08a567edf413
Martin Filliau <martin@filliau.com>
2013-04-23 14:52:16 +0100

    Cleanup unnecessary representations

--- moxie_weather/representations.py
+++ moxie_weather/representations.py
@@ -3,30 +3,6 @@ from flask import url_for, jsonify
 from moxie.core.representations import HALRepresentation
 
 
-class ObservationRepresentation(object):
-
-    def __init__(self, observation):
-        self.observation = observation
-
-    def as_dict(self):
-        return self.observation.as_dict()
-
-    def as_json(self):
-        return jsonify(self.as_dict())
-
-
-class ForecastRepresentation(object):
-
-    def __init__(self, forecast):
-        self.forecast = forecast
-
-    def as_dict(self):
-        return self.forecast.as_dict()
-
-    def as_json(self):
-        return jsonify(self.as_dict())
-
-
 class HALWeatherRepresentation(object):
 
     def __init__(self, observation, forecasts, attribution, last_updated, endpoint):
@@ -39,10 +15,10 @@ class HALWeatherRepresentation(object):
     def as_dict(self):
         values = {}
         if self.observation:
-            values['observation'] = ObservationRepresentation(self.observation).as_dict()
+            values['observation'] = self.observation.as_dict()
         else:
             values['observation'] = None
-        values['forecasts'] = [ForecastRepresentation(f).as_dict() for f in self.forecasts]
+        values['forecasts'] = [f.as_dict() for f in self.forecasts]
         values['_attribution'] = self.attribution
         values['last_updated'] = self.last_updated
         representation = HALRepresentation(values)
