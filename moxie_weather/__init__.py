8ccc50a8b36aaad7577eceb41714152dcf1c26bc
Martin Filliau <martin@filliau.com>
2013-04-23 10:23:20 +0100

    Reduced to one view

--- moxie_weather/__init__.py
+++ moxie_weather/__init__.py
@@ -1,28 +1,9 @@
-from flask import Blueprint, request
-from flask.helpers import make_response
+from flask import Blueprint
 
-from moxie.core.representations import HALRepresentation
-from .views import ForecastsView, ObservationView
+from .views import WeatherView
 
 
 def create_blueprint(blueprint_name, conf):
     weather_blueprint = Blueprint(blueprint_name, __name__, **conf)
-
-    weather_blueprint.add_url_rule('/', view_func=get_routes)
-    weather_blueprint.add_url_rule('/observation',
-                                   view_func=ObservationView.as_view('observation'))
-    weather_blueprint.add_url_rule('/forecast',
-                                   view_func=ForecastsView.as_view('forecast'))
+    weather_blueprint.add_url_rule('/', view_func=WeatherView.as_view('weather'))
     return weather_blueprint
-
-
-def get_routes():
-    path = request.path
-    representation = HALRepresentation({})
-    representation.add_curie('hl', 'http://moxie-weather.readthedocs.org/en/latest/http_api/weather.html#{rel}')
-    representation.add_link('self', '{bp}'.format(bp=path))
-    representation.add_link('hl:observation', '{bp}observation'.format(bp=path), title='Observation')
-    representation.add_link('hl:forecast', '{bp}forecast'.format(bp=path), title='Forecast')
-    response = make_response(representation.as_json(), 200)
-    response.headers['Content-Type'] = "application/json"
-    return response
--- moxie_weather/views.py
+++ moxie_weather/views.py
@@ -7,7 +7,7 @@ from moxie_weather.services import WeatherService
 from moxie_weather.representations import HALObservationRepresentation
 
 
-class ObservationView(ServiceView):
+class WeatherView(ServiceView):
 
     def handle_request(self):
         service = WeatherService.from_context()
@@ -18,7 +18,3 @@ class ObservationView(ServiceView):
     @accepts(HAL_JSON, JSON)
     def as_hal_json(self, result):
         return HALObservationRepresentation(result, self.attribution, request.url_rule.endpoint).as_json()
-
-
-class ForecastsView(ServiceView):
-    pass
